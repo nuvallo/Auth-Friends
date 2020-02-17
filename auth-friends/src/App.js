3873bd2c93465f5d4604348d6cccd4b0601e9f6b
nuvallo <nuvallo.inc@gmail.com>
2020-02-17 13:54:42 -0500

    Installed Reactstrap & created Navigation component

--- auth-friends/src/App.js
+++ auth-friends/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import NavBar from "./components/Navigation";
 import "./App.css";
 
 function App() {
-  return null;
+  return <NavBar />;
 }
 
 export default App;
---
+++ auth-friends/src/components/Navigation.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+
+const NavBar = () => {
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Auth Friends</NavbarBrand>
+
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+            <NavLink href="/components/">Login</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavBar;
